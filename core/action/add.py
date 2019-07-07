9961aaf20ccc0b87ecb396fce6dbf5ccfa07c89d
caostorm <caostorm@163.com>
2019-07-07 10:28:36 +0800

    添加设备增加tag，list暂时还无法通过tag进行筛选和过滤

--- core/action/add.py
+++ core/action/add.py
@@ -4,6 +4,7 @@ import getopt
 from core.interface.action import server_action
 from core.helper.usage import usage_helper
 from core.helper.parser import config_parser
+import re
 
 class action_add(server_action):
     _parameters = [
@@ -31,6 +32,8 @@ class action_add(server_action):
             self._usage()
             exit()
         self._name=None
+        prog_with_value = re.compile('^[\w]+=[0-9a-zA-Z-_]+$')
+        prog_without_value = re.compile('^[\w]+$')
         for opt,arg in opts:
             if opt == '-u':
                 self._user = arg
@@ -45,7 +48,24 @@ class action_add(server_action):
                 exit()
             elif opt == '-n':
                 self._name = arg
+            elif opt == '-t':
+                if not hasattr(self, '_tag'):
+                    self._tag=[]
+                if prog_with_value.match(arg) is not None:
+                    # 带值的标签，例如tag=hello
+                    name,value = arg.split('=')
+                    arg_string='%s="%s"'%(name,value)
+                    self._tag.append(arg_string)
+                elif prog_without_value.match(arg) is not None:
+                    # 不带值的标签，例如tag
+                    arg_string='%s=""'%arg
+                    self._tag.append(arg_string)
+                else:
+                    print("%s is bad value"%(arg))
+                    pass
 
     def run(self):
         config = config_parser()
         config.add_record(self._ip, self._port, self._user, self._password, self._name)
+        code="config.add_tag(self._ip,%s)"%(','.join(self._tag))
+        eval(code,globals(), locals())
--- core/action/list.py
+++ core/action/list.py
@@ -11,12 +11,14 @@ class action_list(server_action):
     # 参数列表示例，可以在这边增加参数描述，最终代码将根据argname生成对对应的参数
     _parameters = [
         {"name":"h", "needarg":False, "desc":"显示这条帮助信息", "argname":"help"},
-        {"name":"n", "needarg":True, "desc":"根据服务器名称进行模糊搜索", "argname":"name"}
+        {"name":"n", "needarg":True, "desc":"根据服务器名称进行模糊搜索", "argname":"name"},
+        {"name":"t", "needarg":True, "desc":"根据标签进行搜索", "argname":"tag"}
     ]
 
     def __init__(self):
         # 创建帮助信息
         self._usage_helper = usage_helper(sys.argv[0], "list", self._parameters)
+        self._config = config_parser()
 
     def _usage(self):
         # 输出action的帮助信息
@@ -60,37 +62,69 @@ class action_list(server_action):
             self._usage()
             exit()
         # ToDo: 自定义的解析方法
-        pass
+        self._tag=[]
+        prog_with_value = re.compile(r'^[\w]+=[0-9a-zA-Z-_]+$')
+        prog_without_value = re.compile(r'^[\w]+$')
+        for opt, arg in opts:
+            if opt == '-t':
+                if prog_with_value.match(arg) is not None:
+                    # 带值的标签，例如tag=hello
+                    name,value = arg.split('=')
+                    self._tag.append({name:value})
+                elif prog_without_value.match(arg) is not None:
+                    # 不带值的标签，例如tag
+                    self._tag.append({arg:''})
+                else:
+                    print("%s is bad value"%(arg))
 
+    # 根据服务器名称进行模糊搜索，如果没有要求名称搜索，则返回全集
     def _search_by_name(self):
-        config = config_parser()
         ret_array = []
         # 不需要使用名称进行过滤时，返回全集
         if self._name == None:
-            for i in config:
+            for i in self._config:
                 ret_array.append(i['ip'])
             return set(ret_array)
-        # 否则进行模糊搜索
+        # 否则进行模糊搜索，把搜索出来的ip放到集合
         prog = re.compile('^.*%s.*$'%(self._name))
-        for i in config:
+        for i in self._config:
             if 'name' not in i:
                 continue
             if prog.match(i['name']) != None:
                 ret_array.append(i['ip'])
         return set(ret_array)
 
+    # 根据服务器的标签进行搜索，如果没有要求标签进行搜索，则返回全集
+    def _search_by_tag(self):
+        ret_array = []
+        # 没有使用tag筛选，返回全集
+        if len(self._tag) == 0:
+            for i in self._config:
+                ret_array.append(i['ip'])
+            return set(ret_array)
+        # 格式化tag输入参数
+        for i in self._config:
+            if 'tags' in i:
+                print(i['tags'])
+        return set([])
+        pass
+
     # action实际执行的动作，请将action的行为添加到这个方法内
     def run(self):
+        # 先按照参数过滤
         name_set = self._search_by_name()
-        config = config_parser()
+        tag_set = self._search_by_tag()
+        finnal_set = name_set & tag_set
         prog = re.compile('^%s$'%(self._name))
-        disp = PrettyTable(["IP", "服务器名称"])
-        for i in config:
+        disp = PrettyTable(["IP", "服务器名称","标签"])
+        for i in self._config:
             # 检测记录是否在搜索结果内
-            if i['ip'] in name_set:
-                if 'name' in i:
-                    disp.add_row([i['ip'], i['name']])
-                else:
-                    disp.add_row([i['ip'], ''])
+            if i['ip'] in finnal_set:
+                name = i['name'] if 'name' in i else ''
+                tag = []
+                if 'tags' in i:
+                    for t in i['tags']:
+                        tag.append("%s:%s"%(t, i['tags'][t]))
+                disp.add_row([i['ip'], name, ','.join(tag)])
         print(disp)
         
